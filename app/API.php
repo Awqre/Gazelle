550724fef2dc53b0bb50322a9fcaffc44b33b29e
Awqre <32497511+Awqre@users.noreply.github.com>
2024-10-16 19:42:15 +0000

    Update API.php
    space -> tab

--- app/API.php
+++ app/API.php
@@ -3,22 +3,22 @@
 namespace Gazelle;
 
 class API extends Base {
-    public function validateToken(int $appId, string $token): bool {
-        $key = "api_applications_{$appId}";
-        $app = self::$cache->get_value($key);
-        if ($app === false) {
-            $app = self::$db->rowAssoc("
-                SELECT Token, Name
-                FROM api_applications
-                WHERE ID = ?
-                LIMIT 1
-                ", $appId
-            );
-            if (is_null($app)) {
-                return false;
-            }
-            self::$cache->cache_value($key, $app, 0);
-        }
-        return $app['Token'] === $token;
-    }
+	public function validateToken(int $appId, string $token): bool {
+		$key = "api_applications_{$appId}";
+		$app = self::$cache->get_value($key);
+		if ($app === false) {
+			$app = self::$db->rowAssoc("
+				SELECT Token, Name
+				FROM api_applications
+				WHERE ID = ?
+				LIMIT 1
+				", $appId
+			);
+			if (is_null($app)) {
+				return false;
+			}
+			self::$cache->cache_value($key, $app, 0);
+		}
+		return $app['Token'] === $token;
+	}
 }
